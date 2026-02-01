f9163eccf10a556cfcb2e7b79d850261af203bef
Coding-Crusader <ungabungaa69@gmail.com>
2026-02-01 22:09:44 +0530

    Add files via upload

---
+++ c31.c
@@ -0,0 +1,14 @@
+// A function to print good morning good afternoon good evening
+#include<stdio.h>
+void goodmorning(){
+    printf("good morning\n");}
+void goodafternoon(){
+    printf("good afternoon\n");}
+void goodnight(){
+    printf("good night\n");}
+int main(){
+    goodmorning();
+    goodafternoon();
+    goodnight();
+    return 0;
+}
